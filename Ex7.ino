50cc47dd33ac5d4414b9aba00b9213f06520ff76
Davi Gomes <davigomes.m1@gmail.com>
2023-04-17 19:31:58 -0300

    Update Ex7.ino

--- Ex7.ino
+++ Ex7.ino
@@ -4,33 +4,39 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("Digite um numero");
+  Serial.begin(9600);
 }
 
 void loop() {
   
   int numero;
-
+  lcd.setCursor(0, 0);
+  lcd.print("Digite um numero");		
   lcd.setCursor(0, 1);
-  lcd.print("Entre 12 e 20:");
+  lcd.print("entre 12 e 20:");
   while (true) {
     if (Serial.available() > 0) {
       numero = Serial.parseInt();
       if (numero >= 12 && numero <= 20) {
         break;
       } else {
+        lcd.clear();
         lcd.setCursor(0, 1);
         lcd.print("Entrada invalida ");
-        delay(1000);
+        delay(3000);
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Digite um numero");
         lcd.setCursor(0, 1);
-        lcd.print("Entre 12 e 20:  ");
+        lcd.print("entre 12 e 20:");
       }
     }
   }
-
-  lcd.setCursor(0, 1);
+  lcd.clear();
+  lcd.setCursor(0, 0);
   lcd.print("Numero digitado: ");
+  lcd.setCursor(0, 1);
   lcd.print(numero);
-  delay(2000);
+  delay(3000);
   lcd.clear();
 }
